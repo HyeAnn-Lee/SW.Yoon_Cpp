60b404848d43262e589fa41556ea8f548d3a936a
HyeAnn <60637683+happy1004ann@users.noreply.github.com>
2020-07-16 17:04:56 +0900

    Create 1_SimpleAdder.cpp

---
+++ Chapter1/1_SimpleAdder.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+  int val1;
+  // double val1;
+  std::cout<<"Input first number: ";
+  std::cin>>val1;
+  
+  int val2;
+  // double val2;
+  std::cout<<"Input second number: ";
+  std::cout>>val2;
+  
+  int result=val1+val2;
+  // double result=val1+val2;
+  std::cout<<"Result: "<<result<<std::endl;
+  return 0;
+}
