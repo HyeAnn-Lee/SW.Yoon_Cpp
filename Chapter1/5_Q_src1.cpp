d2f18c2d0905b9e88a7eb84ff74de6f75dd7599a
HyeAnn <60637683+happy1004ann@users.noreply.github.com>
2020-07-23 17:14:41 +0900

    Create 5_Q_src1.cpp

---
+++ Chapter1/5_Q_src1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "5_Q.h"
+
+namespace BestComImpl{
+    void SimpleFunc(void){
+        std::cout << "func defined by BestCom\n";
+    }
+}
+
+namespace ProgComImpl{
+    void SimpleFunc(void){
+        std::cout << "func defined by ProgCom\n";
+    }
+}
