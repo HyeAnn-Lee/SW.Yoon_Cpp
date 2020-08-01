1a6a77ec80ce01683bdf0d2e9676894ea6188c1c
HyeAnn <60637683+happy1004ann@users.noreply.github.com>
2020-08-01 20:02:10 +0900

    Create Q02-1_1.cpp

---
+++ Chapter02/Q02-1_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void PlusByRef(int &ref){
+    ref++;
+}
+
+void RevByRef(int &ref){
+    ref = -ref;
+}
+
+int main(void){
+    int num = 10;
+    cout << "num: " << num << endl;
+
+    PlusByRef(num);
+    cout << "After +1: " << num << endl;
+
+    RevByRef(num);
+    cout << "Even after changing sign: " << num << endl;
+
+    return 0;
+}
