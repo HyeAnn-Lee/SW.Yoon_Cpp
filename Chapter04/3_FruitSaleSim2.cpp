a4641accf47a81158fc7ba4c0f089665b2785bd7
HyeAnn <60637683+happy1004ann@users.noreply.github.com>
2020-08-05 16:55:56 +0900

    Create 3_FruitSaleSim2.cpp

---
+++ Chapter04/3_FruitSaleSim2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class FruitSeller{
+    private:
+        int APPLE_PRICE;
+        int numOfApples;
+        int myMoney;
+    public:
+        FruitSeller(int price, int num, int money){
+            APPLE_PRICE = price;
+            numOfApples = num;
+            myMoney = money;
+        }
+        int SaleApples(int money){
+            int num = money/APPLE_PRICE;
+            numOfApples -= num;
+            myMoney += money;
+            return num;
+        }
+        void ShowSalesResult(){
+            cout << "Apple left: " << numOfApples << endl;
+            cout << "Margin: " << myMoney << endl << endl;
+        }
+};
+
+class FruitBuyer{
+    int myMoney;
+    int numOfApples;
+
+    public:
+        FruitBuyer(int money){
+            myMoney = money;
+            numOfApples = 0;
+        }
+        void BuyApples(FruitSeller& seller, int money){
+            numOfApples += seller.SaleApples(money);
+            myMoney -= money;
+        }
+        void ShowBuyResult(){
+            cout << "Balance: " << myMoney << endl;
+            cout << "The number of apples: " << numOfApples << endl << endl;
+        }
+};
+
+int main(void){
+    FruitSeller seller(1000, 20, 0);
+    FruitBuyer buyer(5000);
+    buyer.BuyApples(seller, 2000);
+
+    cout << "Status of fruit seller" << endl;
+    seller.ShowSalesResult();
+    cout << "Status of fruit buyer" << endl;
+    buyer.ShowBuyResult();
+    return 0;
+}
