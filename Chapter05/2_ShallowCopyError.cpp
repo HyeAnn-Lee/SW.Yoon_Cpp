b19ba4d0d875cc822c5d8e449aefe29702099e0e
HyeAnn <60637683+happy1004ann@users.noreply.github.com>
2020-08-09 20:57:02 +0900

    Create 2_ShallowCopyError.cpp

---
+++ Chapter05/2_ShallowCopyError.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person{
+    private:
+        char* name;
+        int age;
+    public:
+        Person(const char* myname, int myage){
+            int len = strlen(myname) + 1;
+            name = new char[len];
+            strcpy(name, myname);
+            age = myage;
+        }
+        void ShowPersonInfo() const{
+            cout << "Name: " << name << endl;
+            cout << "Age: " << age << endl;
+        }
+        ~Person(){
+            delete []name;
+            cout << "called destructor!\n";
+        }
+};
+
+int main(void){
+    Person man1("HyeAnn Lee", 23);
+    Person man2 = man1;
+    man1.ShowPersonInfo();
+    man2.ShowPersonInfo();
+    return 0;
+}
